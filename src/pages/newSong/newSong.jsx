4c0167b23a3c2d327fa46a1741f9cce68ce4999b
Rafael José Ossorio López <rafaeljoseossoriolopez@gmail.com>
2024-04-12 09:37:52 +0200

    New Song fixed

--- src/pages/newSong/newSong.jsx
+++ src/pages/newSong/newSong.jsx
@@ -45,7 +45,7 @@ export const NewSong = ({ listOfTags, isAdmin, defaultTags }) => {
         <div className='addSongPage'>
             {isAdmin ? (
                 <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                    <Form className='form-container addSongForm mt-5 mx-auto col-10 col-md-3'>
+                    <Form className='form-container addSongForm mt-5 mx-auto col-8 col-xl-6'>
                         <div className="row justify-content-center mx-auto">
                             <div className='mb-3'>
                                 <label htmlFor='inputAddSongName' className='form-label'>
@@ -84,23 +84,21 @@ export const NewSong = ({ listOfTags, isAdmin, defaultTags }) => {
                         <div className='row justify-content-center mx-auto'>
                             <div className='mb-3'>
                                 <label className='form-label d-flex justify-content-center'>Etiquetas:</label>
-                                <div id='checkbox-group' className='d-flex flex-wrap'>
-                                    <div className='mx-auto defaultTags'>
+                                <div id='checkbox-group' className='d-flex justify-content-center flex-wrap mt-3'>
                                         {defaultTags.map((tag, index) => (
-                                            <div key={index} className='form-check form-check-inline'>
+                                            <div key={index} className='form-check form-check-inline mt-1'>
                                                 <Field type='checkbox' name='tags' value={tag} className='form-check-input' />
                                                 <label className='form-check-label'>{tag}</label>
                                             </div>
                                         ))}
-                                    </div>
                                 </div>
                                 <div id='checkbox-group' className='d-flex justify-content-center flex-wrap mt-3'>
-                                    {listOfTags.map((tag, index) => (
-                                        <div key={index} className='form-check form-check-inline'>
-                                            <Field type='checkbox' name='tags' value={tag} className='form-check-input' />
-                                            <label className='form-check-label'>{tag}</label>
-                                        </div>
-                                    ))}
+                                        {listOfTags.map((tag, index) => (
+                                            <div key={index} className='form-check form-check-inline mt-1'>
+                                                <Field type='checkbox' name='tags' value={tag} className='form-check-input' />
+                                                <label className='form-check-label'>{tag}</label>
+                                            </div>
+                                        ))}
                                 </div>
                                 <ErrorMessage name='tags' component='p' className='text-danger'></ErrorMessage>
                             </div>
